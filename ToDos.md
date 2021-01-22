226fc7abe7d6103a967db8856c89d9a9ccdfbaa5
Bob Lund <b.lund@cablelabs.com>
2021-01-22 08:22:58 -0700

    File based, multi-platform config

---
+++ ToDos.md
@@ -0,0 +1,47 @@
+# ToDos
+
+## On boarding
+
+Account creation should be through Brume website. Brume app can be through app. Third party app users should be access token from website.
+
+### Application installation
+1. User goes to brume.occams.solutions and navigates to download page.
+2. Page suggest appropriate download based on user-agent.
+3. User installs package
+
+### New user signin
+1. Using browser, user goes to localhost:8686
+2. User chooses Brume username/password
+3. User supplies email address and billing information.
+4. User receives email with token.
+5. User goes to localhost:8686 and enters token.
+6. App connects to server and setup is confirmed.
+
+## Setup
+
+### Create a share group
+1. User goes to localhost:8686/mygroups
+2. User creates a new group
+
+### Invite members to a group
+1. User goes to localhost:8686/mygroups/group_name
+2. User adds member names
+3. User checks whether Brume should send invite. If so, notification of member addtion is sent to Brume server which will automatically add group (with email notification) or send invite, depending on member setting. Otherwise, user is responsible for notifying member of invite.
+
+### Accept membership
+
+## Usage Scenarios
+
+###
+
+### Syncing member of groups on app startup
+
+### Syncing owned groups on app startup
+
+### Signaling server goes away
+
+## Accounting
+
+### Tracking usage
+
+
--- brume-client.js
+++ brume-client.js
@@ -1,17 +1,77 @@
 "use strict"
 
-if(process.argv.length != 3) {
-  console.log("USAGE: node brume-client.js usename")
+const fs = require('fs')
+      , path = require('path')
+      , jwt = require('jsonwebtoken')
+;
+
+function buildGroupInfo(baseDir, username) {
+  let groupInfo = {
+    sender: {}
+    ,receiver: {}
+  }
+
+  let p = baseDir+username
+
+  let groups = fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+  for(let group of groups) {
+    try {
+      groupInfo.sender[group] = JSON.parse(fs.readFileSync(p+'/'+group+'/.members.json'))
+      p=baseDir
+      let members = fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+      for(let member of members) {
+        if(member != username) {
+          p = baseDir+member
+          groups = fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+          groupInfo.receiver[member] = {groups: groups}
+        }
+      }
+    } catch(e) {
+      console.error('error reading config file:', e)
+      process.exit(1)
+    }
+  }
+  return groupInfo
+}
+
+const osConfigLocs = {
+  linux: '/etc/brume/brume.conf'
+  ,win32: process.env.PROGRAMDATA + '/brume.conf'
+  ,darwin: '/usr/local/etc/brume/brume.conf'
+}
+
+var configFile
+
+if(process.argv.length == 3) {
+  configFile = process.argv[2]
+} else if(process.env.BRUME_CONFIG) {
+  configFile = process.env.BRUME_CONFIG
+} else configFile = osConfigLocs[process.platform]
+
+try {
+  fs.statSync(configFile).isFile()
+} catch(e) {
+  console.error(`no brume config at ${configFile}`)
   process.exit(1)
 }
 
-const configFile = "./" + process.argv[2] +"-brume-config.js"
-const {token, username, baseDir, groupInfo} = require(configFile)
+try {
+  var {baseDir} = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
+  baseDir = baseDir.replace('./','')
+  var {token, url} = JSON.parse(fs.readFileSync(baseDir + '/.wsserver.json', 'utf-8'))
+  url = 'ws://localhost:8080'
+} catch(e) {
+  console.error('error reading config file:', e)
+  process.exit(1)
+}
+
+const {username} = jwt.decode(token)
+const groupInfo = buildGroupInfo(baseDir, username)
 const createWebsocket = require('./websocket.js')
 
-const awsBrumeServer = "wss://wkiw3ej74c.execute-api.us-east-1.amazonaws.com/Prod"
-      , localBrumeServer = "ws://localhost:8080"
-;
+//const awsBrumeServer = "wss://wkiw3ej74c.execute-api.us-east-1.amazonaws.com/Prod"
+//      , localBrumeServer = "ws://localhost:8080"
+//;
 
 async function main() {
   var sender = require("./sender.js")
@@ -19,7 +79,7 @@ async function main() {
   ;
 
   try {
-    var ws = await createWebsocket(localBrumeServer, username, token)
+    var ws = await createWebsocket(url, username, token)
     var PeerConnection = require('./makePeerConnection.js')(ws, username)
     sender({
       PeerConnection: PeerConnection
