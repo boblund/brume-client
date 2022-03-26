438928330c9d1b78e4b0f90c5e6398ec4973a6ba
Bob Lund <b.lund@cablelabs.com>
2022-03-26 07:56:37 -0600

    simplify

--- logger.js
+++ logger.js
@@ -1,30 +1,36 @@
-"use strict";
+'use strict';
 
-const levels = { ERROR: 0, WARN: 1, INFO: 2, DEBUG: 3 }
-			,strings = Object.keys(levels)
-			,optionsDefaults = { currLevel: levels.INFO, timestamp: process.stdout.isTTY }
-			//,optionsDefaults = { currLevel: levels.INFO, isTTY: process.stdout.isTTY }
+const 
+	levels = ['DEBUG', 'INFO', 'WARN', 'ERROR'],
+	timestamp = process.stdout.isTTY;
+	
+let level = levels.indexOf('INFO');
 
-var {currLevel, timestamp} = optionsDefaults
-
-function log(level, ...args){
-	if(level <= currLevel) {
-		if(timestamp) {
-			let d = new Date()
-  		process.stdout.write(`${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}`
-    		+ ` ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} `)
+const log = a => (...b) => {
+	if(levels.indexOf(a) > -1) {
+		if(levels.indexOf(a) >= level) {
+			if(timestamp) {
+				let d = new Date()
+				process.stdout.write(`${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}`
+					+ ` ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} `)
+			}
+			console.log('['+a+']', ...b);
 		}
-		console.log(`[${strings[level]}]`, ...args)
+	} else {
+		console.log('[ERROR] log: bad log level - log(', a , ') (', ...b, ')');
 	}
-}
+
+};
+
+const setLevel = l => 
+	level = levels.indexOf(l) > -1 
+		? levels.indexOf(l)
+		: (console.log(`[error] log: bad log level log(${a})(${b})`), level);
 
 module.exports = {
-	debug (...args) { log(levels.DEBUG, ...args) }
-	,info (...args) { log(levels.INFO, ...args) }
-	,warn (...args) { log(levels.WARN, ...args) }
-	,error (...args) { log(levels.ERROR, ...args) }
-	,setOptions ({level, timestamp: _timestamp}) {
-		currLevel = level ? levels[level] >= levels.ERROR && levels[level] <= levels.DEBUG ? levels[level] : levels.INFO : currLevel
-		timestamp =  _timestamp == 'ON' ? true : _timestamp == 'OFF' ? false : timestamp
-	}
+	debug(...args) {log('DEBUG')(...args)},
+	info(...args) {log('INFO')(...args)},
+	warn(...args) {log('WARN')(...args)},
+	error(...args) {log('ERROR')(...args)},
+	setLevel
 }
