e458a43d7be6ee1c7a51a2baaa23256dbb5944c9
Bob <bob.lund@gmail.com>
2024-11-08 07:59:18 -0700

    eslinted

--- brumeReceiver.mjs
+++ brumeReceiver.mjs
@@ -1,49 +1,49 @@
-import {readFileSync, writeFileSync} from 'fs';
-import {Brume} from './Brume.mjs';
+import { readFileSync, writeFileSync } from 'fs';
+import { Brume } from './Brume.mjs';
 
 const configFile = process.argv.length == 3
 	? process.argv[2]
 	: process.env.BRUME_CONFIG
 		? process.env.BRUME_CONFIG
-		: process.env.HOME+'/Brume/brume.conf';
+		: process.env.HOME + '/Brume/brume.conf';
 	
-const config = JSON.parse(readFileSync(configFile, 'utf-8'));
+const config = JSON.parse( readFileSync( configFile, 'utf-8' ) );
 config.url = process.env.BRUME_SERVER ? process.env.BRUME_SERVER : 'wss://brume.occams.solutions/Prod';
 
-(async function () {
+( async function () {
 	try{
-		if(!config?.token || !config?.url) throw('token or url not set');
-		const brume = new Brume(config);
+		if( !config?.token || !config?.url ) throw( 'token or url not set' );
+		const brume = new Brume( config );
 
-		brume.on('reauthorize', newconfig => {
-			Brume.log.debug('reauthorize');
-			writeFileSync(configFile, JSON.stringify({...newconfig, url: config.url}));
-		});
+		brume.on( 'reauthorize', newconfig => {
+			Brume.log.debug( 'reauthorize' );
+			writeFileSync( configFile, JSON.stringify( { ...newconfig, url: config.url } ) );
+		} );
 
-		brume.on('serverclose', () => {
-			Brume.log.debug('server restart');
-			setTimeout(async ()=>{ await brume.start(); }, 10*1000);
-		});
+		brume.on( 'serverclose', () => {
+			Brume.log.debug( 'server restart' );
+			setTimeout( async ()=>{ await brume.start(); }, 10 * 1000 );
+		} );
 
-		brume.on('error', e => {
-			Brume.log.debug.error(JSON.stringify(e));
-		});
+		brume.on( 'error', e => {
+			Brume.log.debug.error( JSON.stringify( e ) );
+		} );
 
 		await brume.start();
-		Brume.log.info(`${brume.thisUser} connected to Brume server`);
-		brume.onconnection = async ({peer, accept}) => {
-			peer.on('data', _data => {
+		Brume.log.info( `${ brume.thisUser } connected to Brume server` );
+		brume.onconnection = async ( { peer, accept } ) => {
+			peer.on( 'data', _data => {
 				const data = Brume.decodeMsg( _data );
 				Brume.log.info( `Message from ${ peer.peerUsername }: ${ JSON.stringify( data ) }` ); 
 				peer.send( Brume.encodeMsg( { type: 'msg', data: 'Howdy' } ) );
 			} );
-			peer.on('close', () => { 
-				Brume.log.debug(`peer closed`);
-				process.exit(0); });
-			peer.on('error', ( e ) => { Brume.log.debug(`peer error`); });
+			peer.on( 'close', () => { 
+				Brume.log.debug( `peer closed` );
+				process.exit( 0 ); } );
+			peer.on( 'error', ( e ) => { Brume.log.debug( `peer error` ); } );
 			await accept(); //accept connection
 		};
-	}catch(e){
-		Brume.log.debug(`error: ${e.message}`);
+	}catch( e ){
+		Brume.log.debug( `error: ${ e.message }` );
 	}
-})();
+} )();
--- brumeSender.mjs
+++ brumeSender.mjs
@@ -1,57 +1,57 @@
-import {readFileSync, writeFileSync} from 'fs';
-import {Brume} from './Brume.mjs';
+import { readFileSync, writeFileSync } from 'fs';
+import { Brume } from './Brume.mjs';
 
-function delay(msec){
-	return new Promise(res => {
-		setTimeout(() => res(), msec);
-	});
+function delay( msec ){
+	return new Promise( res => {
+		setTimeout( () => res(), msec );
+	} );
 }
 
 const configFile = process.argv.length == 3
 	? process.argv[2]
 	: process.env.BRUME_CONFIG
 		? process.env.BRUME_CONFIG
-		: process.env.HOME+'/Brume/brume.conf';
+		: process.env.HOME + '/Brume/brume.conf';
 
-const config = JSON.parse(readFileSync(configFile, 'utf-8'));
+const config = JSON.parse( readFileSync( configFile, 'utf-8' ) );
 config.url = process.env?.BRUME_SERVER ? process.env.BRUME_SERVER : 'wss://brume.occams.solutions/Prod';
 		
-(async function () {
+( async function () {
 	let notdone = true;
 	try {
-		if(!config?.token || !config?.url) throw('token or url not set');
-		const brume = new Brume(config); //(config);
+		if( !config?.token || !config?.url ) throw( 'token or url not set' );
+		const brume = new Brume( config ); //(config);
 
-		brume.on('reauthorize', newconfig => {
-			Brume.log.debug('reauthorize');
-			writeFileSync(configFile, JSON.stringify({...newconfig, url: config.url}));
-		});
+		brume.on( 'reauthorize', newconfig => {
+			Brume.log.debug( 'reauthorize' );
+			writeFileSync( configFile, JSON.stringify( { ...newconfig, url: config.url } ) );
+		} );
 
-		brume.on('serverclose', () => {
-			Brume.log.debug('server restart');
-			setTimeout(async ()=>{ await brume.start(); }, 10*1000);
-		});
+		brume.on( 'serverclose', () => {
+			Brume.log.debug( 'server restart' );
+			setTimeout( async ()=>{ await brume.start(); }, 10 * 1000 );
+		} );
 
-		brume.on('error', e => {
+		brume.on( 'error', e => {
 			notdone = false;
-			Brume.log.debug(JSON.stringify(e));
-		});
+			Brume.log.debug( JSON.stringify( e ) );
+		} );
 
 		await brume.start();
-		Brume.log.debug(`${brume.thisUser} connected to Brume server`);
-		const peer = await brume.connect(brume.thisUser == 'sam' ? 'joe' : 'sam');
-		peer.on('data', data => {
+		Brume.log.debug( `${ brume.thisUser } connected to Brume server` );
+		const peer = await brume.connect( brume.thisUser == 'sam' ? 'joe' : 'sam' );
+		peer.on( 'data', data => {
 			Brume.log.info( `Message from ${ peer.peerUsername }: ${ JSON.stringify( Brume.decodeMsg( data ) ) }` );
 			peer.destroy();
 		} );
-		peer.on('close', () => {
-			Brume.log.info(`peer closed`);
+		peer.on( 'close', () => {
+			Brume.log.info( `peer closed` );
 			process.exit( 0 );
 		} );
-		peer.on('error', ( e ) => { Brume.log.debug(`peer error`); });
-		peer.write( Brume.encodeMsg( { type: 'msg', data: 'howdy'} ) );
-		Brume.log.info(`sent message`);
-	} catch(e){
-		Brume.log.debug(`error: ${e.message}`);
+		peer.on( 'error', ( e ) => { Brume.log.debug( `peer error` ); } );
+		peer.write( Brume.encodeMsg( { type: 'msg', data: 'howdy' } ) );
+		Brume.log.info( `sent message` );
+	} catch( e ){
+		Brume.log.debug( `error: ${ e.message }` );
 	}
-})();
+} )();
